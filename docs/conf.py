894dc1086eb6ec824da1b631e718a81b9e9e1bd7
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-04-11 07:36:36 +0000

    #5 Update package version (1.0.0 → 1.1.0)

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 author = "Akio Taniguchi"
 copyright = "2026 Akio Taniguchi"
 project = "ReadonlyDict"
-release = version = "1.0.0"
+release = version = "1.1.0"
 
 autodoc_default_options = {
     "inherited-members": True,
