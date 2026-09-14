21911e80321cf7a2285db793034ae468d536a06b
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-09-14 12:17:41 +0000

    #13 Update package version (2.0.0 → 2.1.0)

--- readonlydict/__init__.py
+++ readonlydict/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["HashableMapping", "Item", "Items", "ReadonlyDict"]
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 # standard library
 from collections.abc import Hashable, Iterable, Iterator, Mapping
